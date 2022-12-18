59eee213339734ffba226a348c66b46d688f2782
Steven Nagle <48779400+Stevennagle@users.noreply.github.com>
2022-12-18 09:26:47 -0500

    Create QuickPathDisplay.bat

---
+++ QuickPathDisplay.bat
@@ -0,0 +1,5 @@
+@echo off
+REM This just spits back the path to whatever you drag and drop on it.
+::  I like to keep it on the Windows Desktop to quickly retrieve the path of a file or directory 
+echo %1
+pause
