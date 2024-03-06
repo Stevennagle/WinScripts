5194fbff221e8d38dd9bcbb8bb1654743f473536
Stevennagle <stevenagle@outlook.com>
2024-03-06 17:00:14 -0500

    Add Win7 TLS 1.2 Functionality

---
+++ Win7_TLS_Registry_Updater/Win7_TLS_RegUP-Run_N_Reboot.bat
@@ -0,0 +1,17 @@
+@echo off
+:: This runs a registry file that adds the requisite directories and values
+:: to the Windows 7 registry for TLS 1.2 and restarts the system
+echo Running C:\Win7_TLS_Registry_Updater\Win7_TLS_RegistryUpdater.reg
+start C:\Win7_TLS_Registry_Updater\Win7_TLS_RegistryUpdater.reg
+echo Exit now to cancel Reboot.
+TIMEOUT /t 20
+shutdown/r
+
+
+
+
+
+
+
+
+
