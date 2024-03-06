8993b262b85aefa5fe23b09243ef2d811c2a2d0e
Steven Nagle <48779400+Stevennagle@users.noreply.github.com>
2024-03-06 17:02:15 -0500

    Updated text

--- Win7_TLS_Registry_Updater/Win7_TLS_RegUP-Run_N_Reboot.bat
+++ Win7_TLS_Registry_Updater/Win7_TLS_RegUP-Run_N_Reboot.bat
@@ -3,7 +3,7 @@
 :: to the Windows 7 registry for TLS 1.2 and restarts the system
 echo Running C:\Win7_TLS_Registry_Updater\Win7_TLS_RegistryUpdater.reg
 start C:\Win7_TLS_Registry_Updater\Win7_TLS_RegistryUpdater.reg
-echo Exit now to cancel Reboot.
+echo Exit now to cancel system reboot.
 TIMEOUT /t 20
 shutdown/r
 
