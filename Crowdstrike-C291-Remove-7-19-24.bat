c316fcd4f4229f57260881666e21a65d32b5253f
Steven Nagle <SteveNagle@Outlook.com>
2024-07-19 10:10:10 -0400

    Fix CrowdStrike Driver Bug c-291

---
+++ Crowdstrike-C291-Remove-7-19-24.bat
@@ -0,0 +1,9 @@
+cls
+@echo off
+title C-00000291*.sys Removal
+echo Running Crowdstrike C-00000291*.sys Removal
+del "C:\Windows\System32\drivers\CrowdStrike\C-00000291*.sys"
+echo Damaged driver C-00000291 has been removed. Rebooting System
+timeout /t 20
+shutdown/r
+exit 0
