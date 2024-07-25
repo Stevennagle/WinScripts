a3ae887bcbbddb60479bf805ce7c48138175c391
Steven Nagle <SteveNagle@Outlook.com>
2024-07-25 15:40:05 -0400

    PowerShell 5.1 PSReadLine rename

---
+++ AutoFixPS51DirReName.ps1
@@ -0,0 +1,15 @@
+#Auto-fix PowerShell directory name error 
+#   Explanation: 
+#-In Windows PowerShell 5.1 the "PSReadline" module was spelled with a lowercase "L."
+#-This was changed to an uppercase "L" for the release of PowerShell 6.
+#-This can create complications with several features.
+#-This directory name mismatch produces errors when attempting to update CmdLets.
+#   How to use this script:
+#-Run from Admin Windows CMD window:
+#C:\WINDOWS\system32> powershell.exe -ExecutionPolicy Bypass -File <yourFilePath>\AutoFixPS51DirName.ps1
+Write-Host "Running Auto-fix for PowerShell 5.1 Directory Name Error"
+cmd /c ren "C:\Program Files\WindowsPowerShell\Modules\PSReadline" PSReadLine
+#   Optional:
+#Remove-Module PSReadline
+Import-Module PSReadLine
+Update-Help -Module PSReadLine -Force
